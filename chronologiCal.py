21b8d90604e94857cf4dd8b19397d192034920d7
Kaikei_e <61903570+Kaikei-e@users.noreply.github.com>
2020-12-26 18:16:16 +0900

    Improved datetime loop processing.

--- chronologiCal.py
+++ chronologiCal.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as ppt
+import datetime
 
 chronological_stmt = "Please enter multiple drinks in chronological order. The order can be in any order. The format should be 'YYYY/MM/DD H:M', such as '2020/12/01 21:34'."
 check_drinks_stmt = "How many drinks do you want to calculate?"
@@ -6,7 +7,7 @@ mode_statement = "Please select a calculation method. Mode 1 is calculated by th
 mode1_statement = "Enter first the amount of the drink and then the amount of caffeine per 100 ml, separated by a space"
 mode2_statement = "Enter the total amount of caffeine in mg."
 
-drinks_list = []
+#drinks_list = []
 selected_mode = None
 
 def number_excp_handler():
@@ -19,8 +20,48 @@ def number_excp_handler():
                 print("Enter an integer greater than or equal to 1.")
             elif number_of_drinks > 0:
                 flag_number = False
+            return number_of_drinks
         except:
             print("An exception occurred.\nPlease enter integer.")
+
+def chrono_excp_handler():
+    flag_chrono = True
+    #print(chronological_stmt)
+    while flag_chrono == True:
+        try:
+            datetimes = input()
+            dts = datetime.datetime.strptime(datetimes,'%Y/%m/%d %H:%M')
+            flag_chrono = False
+            return dts
+        except:
+            print("An exception occurred.\nPlease enter the correct form.")
+
+def yon_excp_handler(y_o_n):
+    flag_yon = True
+    while flag_yon == True:
+        if y_o_n == "y" or y_o_n == "Y":
+            print("Ready to go!")
+            flag_yon = False
+            return False
+        elif y_o_n == "n" or y_o_n == "N":
+            flag_yon = True
+            return True
+
+
+def datetimes_loop(N_o_D):
+    global chronological_stmt
+    dt_list = []
+    flag_datetimes = True
+    print(chronological_stmt)
+    while flag_datetimes == True:
+        for i in range(N_o_D):
+            dt_list = chrono_excp_handler()
+        for i in range(dt_list):
+            print(dt_list[i])
+        print("\nIs this OK?\nEnter 'y' or 'n'.")
+        flag_datetimes = yon_excp_handler(input())
+
+
 
 def mode_excp_handler():
     flag_mode = True
@@ -38,8 +79,9 @@ def mode_excp_handler():
             print("An exception occurred.\nPlease enter 1 or 2.")
 
 
-number_excp_handler()
+datetimes_loop(number_excp_handler())
+
 
 """
-
+Drinks
 """
