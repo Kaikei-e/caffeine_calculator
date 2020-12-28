72d1c5d40404dd2df3c93ce5930d67add2f5c0e1
e_kaikei <e_kaikei@protonmail.com>
2020-12-28 23:18:31 +0900

    Fixed graph drawing.

--- chronologiCal/chronologiCal.py
+++ chronologiCal/chronologiCal.py
@@ -1,7 +1,10 @@
 import sys
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as ppt
 import datetime
+from matplotlib import dates
+
 from funcs import excp_handler
 from funcs import caffe_cals
 from funcs import mode_selecter
@@ -41,8 +44,11 @@ def datetimes_loop(N_o_D):
 def chronological_cal(datetime_list):
     index = 0
     re_time_list = []
+    R_T_L = []
     re_recaffe_list = []
+    R_R_L = []
     rest_of_caffe = 0
+    caffe_sum = 0
     returned_list = []
     caffe_amount = 0
     caffe_caled = 0
@@ -54,57 +60,70 @@ def chronological_cal(datetime_list):
         selected_mode = excp_handler.mode_excp_handler()
         caffe_amount = mode_selecter.mode_selecter(selected_mode)
         if index == 1:
-            returned_list += caffe_cals.compare_calculator(caffe_amount, dt_l, datetime_list[index])
-            if len(returned_list) == 3:
-                caffe_caled += returned_list[2]
-                returned_list.pop(2)
+            re_time_list, re_recaffe_list, rest_of_caffe = caffe_cals.compare_calculator(caffe_amount, dt_l, datetime_list[index])
+            R_T_L.extend(re_time_list)
+            R_R_L.extend(re_recaffe_list)
+            caffe_caled += rest_of_caffe
+            caffe_sum += rest_of_caffe
             
             continue
 
         elif index < len(datetime_list) and index > 1:
-            #re_list = caffe_cals.compare_calculator(caffe_amount, dt_l, datetime_list[index])
-            #returned_list.insert(0, re_list[0])
-            #returned_list.insert(1, re_list[1])
-            #caffe_caled += re_list[2]
             caffe_caled += caffe_amount
-            returned_list += caffe_cals.compare_calculator(caffe_caled, dt_l, datetime_list[index])
-            if (len(returned_list) - 1)  % 2 == 0:
-                caffe_caled = returned_list[index * 2]
-                returned_list.pop(index * 2)
-            
+            re_time_list, re_recaffe_list, rest_of_caffe = caffe_cals.compare_calculator(caffe_caled, dt_l, datetime_list[index])
+            R_T_L.extend(re_time_list)
+            R_R_L.extend(re_recaffe_list)
+            caffe_caled = rest_of_caffe
+            caffe_sum += rest_of_caffe
+
             continue
 
         elif index >= len(datetime_list) - 1:
             caffe_caled += caffe_amount
-            #re_list = caffe_cals.simple_calculator(caffe_caled, dt_l)
-            #returned_list.insert(0, re_list[0])
-            #returned_list.insert(1, re_list[1])
-
-            returned_list += caffe_cals.simple_calculator(caffe_caled, dt_l)
+            caffe_sum += rest_of_caffe
+            re_time_list, re_recaffe_list = caffe_cals.simple_calculator(caffe_caled, dt_l)
+            R_T_L.extend(re_time_list)
+            R_R_L.extend(re_recaffe_list)
+            
             continue
-    return returned_list
+
+    return R_T_L, R_R_L, caffe_sum
+
 
 
 datetime_list = datetimes_loop(excp_handler.number_excp_handler())
 datetime_list = sorted(datetime_list)
 chrono_list = []
 chrono_list = chronological_cal(datetime_list)
-df = pd.DataFrame(chrono_list)
-len_df_cl = len(list(df.columns))
-for i in range(len_df_cl):
-    if i >= len(list(df.columns)):
-        break
-    elif i < len(list(df.columns)):
-        df = df[i] + df[i + 2]
-        df = df[i + 1] + df[i + 2]
-print(df)
-    
-print(returned_list[0])
-print(returned_list[1])
+
+caffe_sum = 0
+dttm_list = []
+caffe_list = []
+dttm_list, caffe_list, caffe_sum = chronological_cal(datetime_list)
+#print(chronological_cal(datetime_list))
+#print(chrono_list)
+
+if caffe_sum < 500:
+    caffe_sum += 200
+else:
+    caffe_sum = caffe_sum * 5 
+
+x1 = [dttm.strftime("%Y/%m/%d %H:%M") for dttm in dttm_list]
+"""
+for dttm in dttm_list:
+    dttm = "{%Y/%m/%d %H:%M}".format(dttm)
+    x1.append(dttm)
+""" 
+y1 = caffe_list
 
 ppt.xlabel("Time(h)")
 ppt.ylabel("Caffeine(mg)")
+#ppt.gca().xaxis.set_major_formatter(dates.DateFormatter("%Y/%m/%d %H:%M"))
+#ppt.gca().xaxis.set_major_locator(dates.HourLocator())
 ppt.grid()
-ppt.plot(returned_list[0], returned_list[1])
-
-
+ppt.plot(x1, y1)
+ppt.xlim(0, len(x1))
+ppt.ylim(0, caffe_sum)
+ppt.gcf().autofmt_xdate()
+ppt.xticks(np.arange(0, len(x1), 21), rotation=45)
+ppt.show()
