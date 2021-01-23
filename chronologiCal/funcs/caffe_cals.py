58b78a479000cbff919b484ddba4e6539bd7ee70
e_kaikei <e_kaikei@protonmail.com>
2021-01-23 19:57:50 +0900

    Commentted out importing pandas.

--- chronologiCal/funcs/caffe_cals.py
+++ chronologiCal/funcs/caffe_cals.py
@@ -3,26 +3,29 @@ import datetime
 time_global = 0
 
 def compare_calculator(caffeine, datetime1, datetime2):
-        time_local = time_global
-        time_list = []
-        caffeine_list = []
-        caffeine = int(caffeine)
-        caffe_decay = 0.97716
+    time_local = time_global
+    time_list = []
+    caffeine_list = []
+    caffeine = int(caffeine)
+    caffe_decay = 0.97716
+    #T_max4 = caffeine
 
-        for i in range(500):
-            if datetime1 >= datetime2:
-                break
-            
-            time_list.append(datetime1)
-            caffeine_list.append(caffeine)
+    #Calculate the amount of caffeine needed for the calculation of T_max
 
-            datetime1 += datetime.timedelta(minutes=10)
-            caffeine = caffeine * caffe_decay
+    for i in range(500):
+        if datetime1 >= datetime2:
+            break
+        
+        time_list.append(datetime1)
+        caffeine_list.append(caffeine)
 
-            if caffeine < 5:
-                break
+        datetime1 += datetime.timedelta(minutes=10)
+        caffeine = caffeine * caffe_decay
 
-        return time_list, caffeine_list, caffeine
+        if caffeine < 5:
+            break
+
+    return time_list, caffeine_list, caffeine
 
 
 def simple_calculator(caffeine, datetime1):
